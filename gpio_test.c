830d685f95a7aefceb2409be87c73b9a10c57833
Robert Pérez <33105729+robpelu@users.noreply.github.com>
2020-01-12 20:43:20 -0300

    Update gpio_test.c

--- gpio_test.c
+++ gpio_test.c
@@ -18,11 +18,9 @@
 
 
 #define GPIO_OUTPUT_IO_23    23
-#define GPIO_OUTPUT_IO_12     12
 #define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_23)
 
 #define T1 "TASK1"
-#define T2 "TASK2"
 #define MAIN "MAIN"
 
 static void gpio_task_example(void* arg)
@@ -32,21 +30,7 @@ static void gpio_task_example(void* arg)
         vTaskDelay(500 / portTICK_RATE_MS);
         gpio_set_level(GPIO_OUTPUT_IO_23, 0);
         vTaskDelay(500 / portTICK_RATE_MS);
-	//ESP_LOGI(T1, "Tarea de parpadeo de LED's corriendo...");
-    }
-
-}
-
-
-static void gpio_task_read(void* arg)
-{
-    while(1) {
-        if(gpio_get_level(GPIO_OUTPUT_IO_12) == 0)
-        {
-		ESP_LOGI(T2, ".");
-	}
-	else
-    		vTaskDelay(10 / portTICK_RATE_MS);
+	ESP_LOGI(T1, "Tarea de parpadeo de LED's corriendo...");
     }
 
 }
@@ -68,31 +52,12 @@ void app_main()
     gpio_config(&io_conf);
 
 
-
-    //gpio_config_t io_conf1;
-    //disable interrupt
-    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = (1ULL<<GPIO_OUTPUT_IO_12);
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 1;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
-
-
-
     //crear tareas
     xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
-    xTaskCreate(gpio_task_read, "gpio_task_read", 2048, NULL, 10, NULL);
-
     while(1) {
         vTaskDelay(2000 / portTICK_RATE_MS);
-	//ESP_LOGI(MAIN, "Main sigue corriendo en el fondo haciendo nada....");
+	ESP_LOGI(MAIN, "Main sigue corriendo en el fondo haciendo nada....");
     }
 }
 
