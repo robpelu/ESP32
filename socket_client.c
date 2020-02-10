8ec391928ba220b1d3d7f3dffb7cf19d1a60bcdd
Robert Pérez <33105729+robpelu@users.noreply.github.com>
2020-02-10 19:20:36 -0300

    Create socket_client.c

---
+++ socket_client.c
@@ -0,0 +1,194 @@
+/* WiFi station Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include <stdio.h> /* printf, sprintf */
+#include <stdlib.h> /* exit, atoi, malloc, free */
+#include <unistd.h> /* read, write, close */
+
+
+#include <sys/socket.h> /* socket, connect */
+#include <netdb.h> /* struct hostent, gethostbyname */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+
+/* The examples use WiFi configuration that you can set via 'make menuconfig'.
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID      "xxxxx"
+#define EXAMPLE_ESP_WIFI_PASS      "xxxxx"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  4
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+int connect_to_socket(char* host, char* port);
+
+/* The event group allows multiple bits for each event, but we only care about one event 
+ * - are we connected to the AP with an IP? */
+const int WIFI_CONNECTED_BIT = BIT0;
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        ESP_LOGI(TAG, "got ip:%s",
+                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        {
+            if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+                esp_wifi_connect();
+                xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+                s_retry_num++;
+                ESP_LOGI(TAG,"retry to connect to the AP");
+            }
+            ESP_LOGI(TAG,"connect to the AP fail\n");
+            break;
+        }
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+void wifi_init_sta()
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+}
+
+
+int s=0; //socket
+int r=0;
+int bytes_received=0;
+#define BUFFER_SIZE 1000
+char recv_buf[BUFFER_SIZE];
+
+void connect_and_receive()
+{
+    while(1)
+    {
+
+        if(s == 0)
+        {
+            if(connect_to_socket("192.168.1.24", "2000") == 0)
+                ESP_LOGI(TAG, "Conectado a puerto 2000 !");
+        }
+        
+        do {
+            r = read(s, recv_buf + bytes_received , BUFFER_SIZE -1 - bytes_received );
+            bytes_received += r;
+
+        } while(r > 0);
+
+        ESP_LOGI(TAG, "%s", recv_buf);
+        close(s);
+        s=0;
+        r = bytes_received = 0;
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+}
+
+
+
+int connect_to_socket(char* host, char* port)
+{
+    struct addrinfo *res;
+
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    int err=-1;
+    
+    err = getaddrinfo(host, port, &hints, &res);
+    
+    if(err != 0 || res == NULL) {
+        ESP_LOGI(TAG, "DNS lookup to %s:%s failed err=%d res=%p", host, port, err, res);
+        return -1;
+    }
+    
+    s = socket(res->ai_family, res->ai_socktype, 0);
+    if(s < 0) {
+        ESP_LOGI(TAG, "... Failed to allocate socket.");
+        freeaddrinfo(res);
+        return -1;           
+    }
+    
+    if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+        ESP_LOGI(TAG, "... socket connect failed errno=%d", errno);
+        close(s);
+        freeaddrinfo(res);
+        return -1;
+    }
+
+    freeaddrinfo(res);
+    return 0;
+}
+
+
+
+
+void app_main()
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+    xTaskCreate(connect_and_receive, "connect_and_receive", 30000, NULL, 5, NULL);
+
+}
